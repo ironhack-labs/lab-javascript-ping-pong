492e52cca6a4835614709bedaeb2d85288be7ea9
inka43 <enrikeglegarra@gmail.com>
2017-01-27 00:34:07 +0100

    sin comentarios

--- starter-code/js/ball.js
+++ starter-code/js/ball.js
@@ -1,10 +1,17 @@
 function Ball(x,y, paddle1, paddle2) {
+  this.x=x;
+  this.y=y;
+  this.paddle1=paddle1;
+  this.paddle2=paddle2;
 }
 
 Ball.prototype.randomDirection = function() {
+
 };
 
 Ball.prototype.move = function(){
+  var ballon = document.getElement("ball");
+
 };
 
 Ball.prototype.pointScored = function(){
@@ -15,4 +22,30 @@ Ball.prototype.winner = function(){
 };
 
 Ball.prototype.restart = function(){
+};
+
+function GetChar (event){
+            var chCode = ('charCode' in event) ? event.charCode : event.keyCode;
+            alert ("The Unicode character code is: " + chCode);
+
+            switch (chCode) {
+              case 119:
+              
+                  $("#paddle1").css('margin-top', '20px');
+                    chCode = 0;
+                break;
+              case 115:
+                $("#paddle1").css('margin-botton', '20px');
+                chCode = 0;
+                break;
+              default:
+              }
+
+
+        }
+
+$(document).ready = function() {
+
+
+
 };
