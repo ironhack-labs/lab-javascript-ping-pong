2e896914f66d26f8422aa77d64821c90866af07f
ahuertam <ahuertam@yahoo.es>
2017-01-27 00:11:58 +0100

    IT MOVES IT MOVESgit add .!

--- starter-code/js/ball.js
+++ starter-code/js/ball.js
@@ -1,10 +1,12 @@
 function Ball(x,y, paddle1, paddle2) {
+
 }
 
 Ball.prototype.randomDirection = function() {
 };
 
 Ball.prototype.move = function(){
+
 };
 
 Ball.prototype.pointScored = function(){
@@ -16,3 +18,30 @@ Ball.prototype.winner = function(){
 
 Ball.prototype.restart = function(){
 };
+
+
+$(document).ready(function () {
+    var interval = 1000;
+    var $ball =$('#ball');
+    var random= function(){
+      return Math.floor((Math.random()* 4) + 1);
+    };
+    while (interval>0) {
+      for(i=0;i<6;i++){
+      $ball.css('left', $ball.offset().left - 10);
+      }
+      for(i=0;i<6;i++){
+      $ball.css('top', $ball.offset().top - 10);
+      }
+      for(i=0;i<6;i++){
+      $ball.css('left', $ball.offset().left + 10);
+      }
+      for(i=0;i<6;i++){
+      $ball.css('top', $ball.offset().top + 10);
+      interval--;
+      }
+
+    }
+
+
+});
