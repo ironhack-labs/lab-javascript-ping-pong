26205f38020fd4ec8dd4cd2f988f96c6c7a05016
Borjadrr <borja.drr@gmail.com>
2017-01-27 08:51:05 +0100

    Ping Pong Game

--- starter-code/js/ball.js
+++ starter-code/js/ball.js
@@ -1,11 +1,32 @@
 function Ball(x,y, paddle1, paddle2) {
+  this.x = x;
+  this.y = y;
 }
 
 Ball.prototype.randomDirection = function() {
+
+
+
+
 };
 
 Ball.prototype.move = function(){
+
+  var a =  Math.floor(Math.random()*1);
+  var intervalId = setInterval (function(x,y){
+
+  if (a === 0){return x+20;
+  }
+  else if (b === 0) {return y+20;
+  }
+  else {
+    return [x-20,y-20];
+  }
+  }, 1000);
+
 };
+
+
 
 Ball.prototype.pointScored = function(){
 };
