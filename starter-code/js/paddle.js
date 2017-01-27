3d63d339f4d7aae22c17b7fd28cf2eb3ec223c8c
juliapolbach <juliapolbach@gmail.com>
2017-01-27 09:01:28 +0100

    -

--- starter-code/js/paddle.js
+++ starter-code/js/paddle.js
@@ -5,3 +5,12 @@ Paddle.prototype.restart = function(){
 
 Paddle.prototype.hitBall = function(ball_y){
 };
+
+$(document).ready(function() {
+
+  $(document).keypress(function() {
+    console.log( "Handler for .keypress() called." );
+    
+  });
+
+});
