e61efb82c3e0b88cc30674b75c8483aedbf611c6
edsonjmv <edsonjmv@gmail.com>
2017-04-13 21:16:13 +0200

    first commit

--- starter-code/js/ball.js
+++ starter-code/js/ball.js
@@ -1,4 +1,8 @@
 function Ball(x,y, paddle1, paddle2) {
+  this.x = x;
+  this.y = y;
+  this.original_x = x;
+  this.original_y = y;
 }
 
 Ball.prototype.randomDirection = function() {
@@ -15,4 +19,16 @@ Ball.prototype.winner = function(){
 };
 
 Ball.prototype.restart = function(){
+  this.x = this.original_x;
+  this.y = this.original_y;
 };
+
+var movement = setInterval(function(){
+  var ball = document.getElementById('ball'),
+  style = window.getComputedStyle(ball),
+  top = style.getPropertyValue('top');
+  left = style.getPropertyValue('left');
+
+  ball.style.top = parseInt(top.slice(0, -1))-1+"px";
+  ball.style.left = parseInt(left.slice(0, -1))+1+"px";
+}, 5);
