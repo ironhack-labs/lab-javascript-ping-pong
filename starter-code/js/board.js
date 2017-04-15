361a19b8424cf84f02e4ed22feb67c9e3a1cbb84
Hery Martin <scorpionxii@gmail.com>
2017-04-15 15:36:27 +0200

    Done!

--- starter-code/js/board.js
+++ starter-code/js/board.js
@@ -1,17 +1,69 @@
-function Board() {
+function Board(sizeX, sizeY, ball, userPaddle, compPaddle) {
+    this.sizeX = sizeX;
+    this.sizeY = sizeY;
+    this.ball = ball;
+    this.userPaddle = userPaddle;
+    this.compPaddle = compPaddle;
+    this.stopped = true;
+    
+    this.locateBallAtCenter();
 }
 
-Board.prototype.start = function(){
+Board.prototype.doGameStep = function(){
+    this.compPaddleIA();
+    this.checkBallCollision();
+    this.ball.move(); 
 };
 
 Board.prototype.checkGame = function(){
+    
+};
+
+Board.prototype.checkBallCollision = function(){
+    if (this.userPaddle.hitBall(this.ball.x - 8, this.ball.y + 8) || this.compPaddle.hitBall(this.ball.x + 16, this.ball.y + 8)) { //Adding 10 for div corner compensation; 
+        this.ball.directionX *= -1;
+        this.ball.x += 8 * this.ball.directionX;
+        this.ball.newVelocityDeltas();
+    }
+    
+    if (this.ball.x >= this.sizeX - 10) {
+        this.userPaddle.scoredPoints++;
+        this.stop();
+    }
+    
+    if (this.ball.x <= 0) {
+        this.compPaddle.scoredPoints++;
+        this.stop();
+    }
+        
+        
+    if (this.ball.y >= this.sizeY - 10 || this.ball.y <= 0)
+        this.ball.directionY *= -1;  
 };
 
 Board.prototype.stop = function(){
+    this.stopped = true;
 };
 
-Board.prototype.restart = function(){
+Board.prototype.start = function(){
+    this.stopped = false;
+};
+
+Board.prototype.locateBallAtCenter = function(){
+    this.ball.randomDirection();
+    this.ball.x = (this.sizeX / 2) - 8;
+    this.ball.y = (this.sizeY / 2) - 8;
 };
 
 Board.prototype.gameOver = function(){
+    
+};
+
+Board.prototype.compPaddleIA = function(){
+    if(this.ball.x > this.sizeX / 2) {
+        if (this.ball.y < this.compPaddle.y + this.compPaddle.size / 2 && this.compPaddle.y > 0)
+            this.compPaddle.y -= 7;
+        else if (this.ball.y > this.compPaddle.y + this.compPaddle.size / 2 && this.compPaddle.y < this.sizeY - this.compPaddle.size)
+            this.compPaddle.y += 7;
+    }
 };
--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,30 +1,63 @@
-var board = new Board();
+var paddle1 = new Paddle($('#paddle1').position().left, $('#paddle1').position().top, 80);
+var paddle2 = new Paddle($('#paddle2').position().left, $('#paddle2').position().top, 80);
+var ball = new Ball(500, 300);
+var board = new Board(1000, 600, ball, paddle1, paddle2);
+var keys = {};
 
-
-$('#start').on('click', function(){
-  board.start();
-  activatePaddle2();
-  var game = setInterval(updateState, intervalTime);
-  renderGame();
-});
-
-function updateState(){
+function updateState() {
+    checkMenuControls();
+    
+    if (!board.stopped) {
+        board.doGameStep();
+        checkPaddleControls();   
+    } else {
+        board.locateBallAtCenter();
+    }
+    
+    renderGame();
 }
-
-$(document).on('keydown', function(e){
-});
 
 function activatePaddle2() {
 }
 
-function renderGame(){
+function renderGame() {
+    renderBall();
+    renderPaddle();
 }
 
-function renderScore(){
+function renderScore() {
 }
 
-function renderBall(){
+function renderBall() {
+    $('#ball').css("left", ball.x);
+    $('#ball').css("top",  ball.y);
 }
 
-function renderPaddle(){
+function renderPaddle() {
+    $('#paddle1').css("top", paddle1.y);
+    $('#paddle2').css("top", paddle2.y);
 }
+
+function checkPaddleControls() {
+    if (keys[38] && paddle1.y > 0)
+        paddle1.y -= 20;
+      
+    if (keys[40] && paddle1.y < 600 - 80)
+        paddle1.y += 20;    
+}
+
+function checkMenuControls(){
+    if (keys[13]) {
+        board.start();
+    }
+}
+
+$(document).ready(function() {
+    setInterval(updateState, 30);
+        
+    $(document).keydown(function(event){
+        keys[event.which] = true;
+    }).keyup(function(event){
+        delete keys[event.which];
+    });
+});
