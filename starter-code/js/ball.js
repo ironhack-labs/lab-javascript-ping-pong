11d9ae5a94726456da1e6c9940ba23eb846db2e4
Marc Palatsí <mpalatsi@me.com>
2017-06-22 23:03:35 +0200

    First iteration

--- starter-code/js/ball.js
+++ starter-code/js/ball.js
@@ -1,18 +1,93 @@
-function Ball(x,y, paddle1, paddle2) {
+function Ball(x, y, paddle1, paddle2) {
+  this.direction = 1;
 }
 
 Ball.prototype.randomDirection = function() {
+  //firts direction of the game
+  // $(document).keydown(function(event) {
+  //   x = $("#ball").position().top;
+  //   console.log("la posicion de ball respecto a la izq es" + x);
+  //   if (event.keyCode === 40) {
+  //     $("#ball").css({left:x+=20});
+  //   }else if(event.keyCode === 38) {
+  //   }
+  // });
 };
 
-Ball.prototype.move = function(){
+Ball.prototype.move = function(direction) {
+  this.direction = direction;
+  var that = this;
+  var ballSpeed = 2;
+  var intervalId = setInterval(function() {
+    var ballVerticalPosition = $("#ball").position().top;
+    var ballHorizontalPosition = $("#ball").position().left;
+    var paddleVerticalPosition = $("#paddle-user").position().top;
+    var paddleHorizontalPosition = $("#paddle-user").position().left;
+    switch (that.direction) {
+      case 1:
+        // console.log(paddleVerticalPosition, paddleHorizontalPosition, ballVerticalPosition, ballHorizontalPosition);
+        // if ((paddleVerticalPosition < ballVerticalPosition) && (ballVerticalPosition < paddleVerticalPosition + 50) && (ballHorizontalPosition < paddleHorizontalPosition) && (ballHorizontalPosition > paddleHorizontalPosition - 10)) {
+        //   that.direction = 4;
+        // }
+        if (ballVerticalPosition > 2 && ballHorizontalPosition < 898) {
+          $("#ball").css({
+            top: ballVerticalPosition -= ballSpeed,
+            left: ballHorizontalPosition += ballSpeed
+          });
+        } else if (ballHorizontalPosition > 898) {
+          that.direction = 4;
+        } else {
+          that.direction = 2;
+        }
+        break;
+      case 2:
+        if (ballVerticalPosition < 400 && ballHorizontalPosition < 898) {
+          $("#ball").css({
+            top: ballVerticalPosition += ballSpeed,
+            left: ballHorizontalPosition += ballSpeed
+          });
+        } else if (ballHorizontalPosition > 898) {
+          that.direction = 3;
+        } else {
+          that.direction = 1;
+        }
+        break;
+      case 3:
+        if (ballVerticalPosition < 400 && ballHorizontalPosition > 2) {
+          $("#ball").css({
+            top: ballVerticalPosition += ballSpeed,
+            left: ballHorizontalPosition -= ballSpeed
+          });
+        } else if (ballHorizontalPosition < 2) {
+
+          that.direction = 1;
+        } else {
+          that.direction = 4;
+        }
+        break;
+
+      case 4:
+        if (ballVerticalPosition > 2 && ballHorizontalPosition > 2) {
+          $("#ball").css({
+            top: ballVerticalPosition -= ballSpeed,
+            left: ballHorizontalPosition -= ballSpeed
+          });
+
+        } else if (ballHorizontalPosition < 2) {
+
+          that.direction = 1;
+        } else {
+          that.direction = 3;
+        }
+        break;
+
+    }
+  }, 30);
 };
 
-Ball.prototype.pointScored = function(){
-};
+Ball.prototype.pointScored = function() {};
 
 // returns winner paddle or false
-Ball.prototype.winner = function(){
-};
+Ball.prototype.winner = function() {};
 
-Ball.prototype.restart = function(){
-};
+Ball.prototype.restart = function() {};
--- starter-code/js/paddle.js
+++ starter-code/js/paddle.js
@@ -1,7 +1,34 @@
-function Paddle(x,y) {
+function Paddle(x, y) {
+  this.x = x;
+  this.y = y;
 }
-Paddle.prototype.restart = function(){
-};
+Paddle.prototype.restart = function() {};
 
-Paddle.prototype.hitBall = function(ball_y){
-};
+Paddle.prototype.hitBall = function(ball_y) {};
+
+
+//move user paddle and control border movements
+$(document).keydown(function(event) {
+  x = $("#paddle-user").position().top;
+  if (event.keyCode === 40) {
+    //move down
+    if (x < 350) {
+      $("#paddle-user").css({
+        top: x += 5
+      });
+    }
+  } else if (event.keyCode === 38) {
+    //move up
+    if (x < 5) {
+      $("#paddle-user").css({
+        top: x
+      });
+    } else {
+      $("#paddle-user").css({
+        top: x -= 5
+      });
+    }
+  }
+});
+
+// document.addEventListener(“keydown”,moveListener)
