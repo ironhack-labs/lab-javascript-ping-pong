61e6a455afc1354240d1590b62b8b8d7e3fa5bbf
Claudia Duarte <Clau@MacBook-Pro-de-Claudia.local>
2017-06-22 22:25:58 +0200

    MAD Claudia Duarte

--- starter-code/js/board.js
+++ starter-code/js/board.js
@@ -1,17 +1,15 @@
 function Board() {
+
 }
 
-Board.prototype.start = function(){
+Board.prototype.start = function() {
+
 };
 
-Board.prototype.checkGame = function(){
-};
+Board.prototype.checkGame = function() {};
 
-Board.prototype.stop = function(){
-};
+Board.prototype.stop = function() {};
 
-Board.prototype.restart = function(){
-};
+Board.prototype.restart = function() {};
 
-Board.prototype.gameOver = function(){
-};
+Board.prototype.gameOver = function() {};
--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,18 +1,32 @@
 var board = new Board();
 
 
-$('#start').on('click', function(){
+$('.btn-start').on('click', function(){
   board.start();
   activatePaddle2();
   var game = setInterval(updateState, intervalTime);
   renderGame();
-});
-
+  });
 function updateState(){
 }
 
+
 $(document).on('keydown', function(e){
-});
+    var x = $("#paddle1").position().top;
+    console.log($("#paddle1").position());
+  if(e.keyCode === 38){
+      if(x <= 3){
+        return;
+      }
+      $("#paddle1").css({top: x-=15});
+    }else if(e.keyCode === 40){
+      if(x >= 90){
+        return;
+      }
+      $("#paddle1").css({top: x+=15});
+    }
+  });
+
 
 function activatePaddle2() {
 }
