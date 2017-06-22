094f03cf28ffc387c418006dd01984770936f808
Juan Macias <yo@soyjuanmacias.com>
2017-06-22 19:31:13 +0200

    Rigth paddle working yeahgit add .

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -12,6 +12,18 @@ function updateState(){
 }
 
 $(document).on('keydown', function(e){
+  var x = $("#paddle2").position().top;
+
+  console.log(x);
+  if (e.keyCode == 38) {
+    $("#paddle2").css({
+      top: x -= 5
+    });
+  } else if (e.keyCode == 40) {
+    $("#paddle2").css({
+      top: x += 5
+    });
+  }
 });
 
 function activatePaddle2() {
