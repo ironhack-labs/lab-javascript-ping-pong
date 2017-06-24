2c7039df1c45e73a8f398f3d20dba17cd9adcc58
mafevito <mafevito@hotmail.com>
2017-06-24 18:07:36 +0200

    first commit

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,8 +1,11 @@
 var board = new Board();
+var ball = new Ball();
 
 
-$('#start').on('click', function(){
+$('.button').on('click', function(){
+  console.log('lol');
   board.start();
+  ball.move();
   activatePaddle2();
   var game = setInterval(updateState, intervalTime);
   renderGame();
@@ -12,6 +15,20 @@ function updateState(){
 }
 
 $(document).on('keydown', function(e){
+     var x = $('.paddle-red').position().top;
+     console.log($('.paddle-red').position());
+
+    if(event.keyCode === 38){
+       if(x <= 0){
+         return;
+       }
+       $('.paddle-red').css({top: x-=15});
+     }else if(event.keyCode === 40){
+       if(x >= 490){
+         return;
+       }
+       $('.paddle-red').css({top: x+=15});
+     }
 });
 
 function activatePaddle2() {
