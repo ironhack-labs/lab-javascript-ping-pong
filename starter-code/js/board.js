0e973d4510a6823e79c400dd02fa80a230b9dd5d
salvocostahack <salvatorecostanzopa@gmail.com>
2017-06-24 19:54:31 +0200

    first iterations

--- starter-code/js/board.js
+++ starter-code/js/board.js
@@ -1,8 +1,27 @@
 function Board() {
+  this.score = 0;
+  this.score2 = 0;
 }
 
 Board.prototype.start = function(){
+  $(document).on('keydown', function(e){
+    var x = $("#paddle1").position().top;
+    var y = $("#paddle2").position().top;
+   if(event.keyCode === 38){
+      if(x <= 0){
+        return;
+      }
+      $("#paddle1").css({top: x-=15});
+    }else if(event.keyCode === 40){
+      if(x >= 285){
+        return;
+      }
+      $("#paddle1").css({top: x+=15});
+    }
+  });
 };
+
+
 
 Board.prototype.checkGame = function(){
 };
--- starter-code/js/paddle.js
+++ starter-code/js/paddle.js
@@ -1,7 +1,48 @@
 function Paddle(x,y) {
+  this.x = x;
+  this.y = y;
 }
 Paddle.prototype.restart = function(){
+  $("#paddle1").css("top", "150px");
+  $("#paddle2").css("top", "150px");
 };
+
+Paddle.prototype.movePaddle = function(){
+  $(document).on('keydown', function(e){
+    var x = $("#paddle1").position().top;
+   if(event.keyCode === 38){
+      if(x <= 0){
+        return;
+      }
+      $("#paddle1").css({top: x-=15});
+    }else if(event.keyCode === 40){
+      if(x >= 285){
+        return;
+      }
+      $("#paddle1").css({top: x+=15});
+    }
+  });
+};
+
+Paddle.prototype.movePaddleSecond = function(){
+  $(document).on('keydown', function(e){
+    var y = $("#paddle2").position().top;
+   if(event.keyCode === 87){
+      if(y <= 0){
+        return;
+      }
+      $("#paddle2").css({top: y-=15});
+    }else if(event.keyCode === 83){
+      if(y >= 285){
+        return;
+      }
+      $("#paddle2").css({top: y+=15});
+    }
+  });
+};
+
+
+
 
 Paddle.prototype.hitBall = function(ball_y){
 };
