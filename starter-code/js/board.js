16a50b0a787e4209fffe20e8b97faff85a5aec8f
Carlos Dueñas <carlosdss84@gmail.com>
2017-06-25 20:15:34 +0200

    versión control

--- starter-code/js/board.js
+++ starter-code/js/board.js
@@ -7,12 +7,11 @@ function Board() {
   this.player1Score = 0;
   this.player2Score = 0;
   this.ballMoving = 0;
+  this.assignControlsToKeys();
 }
 
 Board.prototype.start = function(){
   this.ballMoving = setInterval(this.ball.move.bind(this), 100);)
-  
-
   /*var check = this.checkGame();
   if (!check){
     this.stop();
@@ -20,16 +19,24 @@ Board.prototype.start = function(){
 };
 
 Board.prototype.checkGame = function(){
+
   if (this.ball.x <= 0){
     this.player2Score += 15;
+    this.stop();
   } else if (this.ball.x >= (this.width - 25)){
     this.player1Score += 15;
-};
+    this.stop();
+  }
+;
 
 Board.prototype.stop = function(){
-  var that = this;
-  clearInterval(that.ballMoving);
-  };
+  console.log("Game stopped");
+  if (this.ballMoving){
+    clearInterval(this.ballMoving);
+    this.ballMoving = undefined;
+  }
+  if (!this.gameOver) {this.restart()};
+}
 
 Board.prototype.restart = function(){
   this.paddle1.restart()
@@ -40,12 +47,29 @@ Board.prototype.restart = function(){
 
 Board.prototype.gameOver = function(player){
   var winner
-  if (this.player1Score > this.player2Score){
-    winner = "Player 1.";
-  }else if (this.player1Score < this.player2Score){
-    winner = "Player 2.";
-  }else {
-    winner = "both players."
-  }
-  alert("Game Over. The winnner is/are: " + winner);
+  if(this.player1Score == 60 || This.player2Score == 60){
+      if (this.player1Score > this.player2Score){
+        winner = "Player 1.";
+      }else if (this.player1Score < this.player2Score){
+        winner = "Player 2.";
+      }else {
+        winner = "both players."
+      }
+      alert("Game Over. The winnner is/are: " + winner);
+      return true;
+    }
+    return false;
 };
+
+Board.prototype.assignControlsToKeys = function(){
+  $('body').on('keydown', function(e) {
+    switch (e.keyCode) {
+      case 38: // arrow up
+        this.paddle1.y -= 5;
+        break;
+      case 40: // arrow down
+        this.paddle1.y += 5;
+        break;
+      }
+    }.bind(this));
+  };
--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,30 +1,59 @@
-var board = new Board();
 
 
-$('#start').on('click', function(){
-  board.start();
-  activatePaddle2();
-  var game = setInterval(updateState, intervalTime);
+function updateState(board){
+  board.chekGame();
+
+  activatePaddle2()
   renderGame();
-});
-
-function updateState(){
 }
 
-$(document).on('keydown', function(e){
-});
-
-function activatePaddle2() {
+function activatePaddle2(board) {
+  var follow = setInterval(board.paddle2.followBall()), 100);
 }
 
 function renderGame(){
+  renderBall();
+  renderPaddle();
+  renderScore();
 }
 
 function renderScore(){
+  $(".score1").html(board.player1Score);
+  $(".score1").html(board.player2Score);
 }
 
 function renderBall(){
+  $(".ball").css({
+    top: board.ball.position.y,
+    left: board.ball.position.x
+  });
 }
 
 function renderPaddle(){
+  $(".paddle1").css({
+    top: board.paddle1.y,
+    left: board.paddle1.x
+  });
+
+  $(".paddle2").css({
+    top: board.paddle2.y,
+    left: board.paddle2.x
+  });
 }
+
+
+$(document).ready(function() {
+
+    $('#start').on('click', function(){
+      var board = new Board
+      board.start();
+      var game = setInterval(updateState, 100);
+
+
+
+      activatePaddle2();
+
+
+  });
+  game.start();
+});
