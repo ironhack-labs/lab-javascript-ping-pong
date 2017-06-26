2fb5f6cb7bd00671901521923aaf62f6e80acd4a
Victor Rodriguez <victor.irix@gmail.com>
2017-06-26 07:36:58 +0200

    Not Working

--- starter-code/js/ball.js
+++ starter-code/js/ball.js
@@ -1,11 +1,95 @@
 function Ball(x,y, paddle1, paddle2) {
+
+    this.paddel1 = paddle1;
+    this.paddle2 = paddle2;
+    this.direction = 0;
+    // 0 = upRight  || 1 = downRight
+    // 2 = downLeft || 3 = upLeft
 }
 
 Ball.prototype.randomDirection = function() {
 };
 
 Ball.prototype.move = function(){
+  var x = $("#ball").position().left;
+  var y = $("#ball").position().top;
+  var direction = 0;
+  var that=this;
+  var vel = 10;
+
+  var intervalId = setInterval(function(){
+    console.log(x,y);
+    switch (that.direction) {
+      case 0:
+        if (y > 0) {
+          console.log(that.direction);
+          $("#ball").css({left: x+=vel});
+          $("#ball").css({top: y-=vel});
+          } else {
+        that.direction = 1;
+        }
+        break;
+      case 1:
+        if (y < 390 ){
+          console.log(that.direction);
+          $("#ball").css({left: x+=vel});
+          $("#ball").css({top: y+=vel});
+          if (x > 790) {
+            $("#ball").css({left: x+=vel});
+            $("#ball").css({top: y+=vel});
+            }
+        } else {
+          that.direction = 0;
+        }
+        break;
+      case 2:
+        if (y < 390){
+          console.log(that.direction);
+          $("#ball").css({left: x-=vel});
+          $("#ball").css({top: y+=vel});
+        } else {
+        that.direction = 3;
+        }
+        break;
+      case 3:
+      if (y > 0){
+        console.log(that.direction);
+        $("#ball").css({left: x-=vel});
+        $("#ball").css({top: y-=vel});
+        } else {
+        that.direction = 2;
+        }
+        break;
+    }
+  }, 100);
 };
+
+
+/*Ball.prototype.move = function(){
+  var x = $("#ball").position().left;
+  var y = $("#ball").position().top;
+  var direction = 0;
+  var that=this;
+
+  var intervalId = setInterval(function(){
+    console.log(x,y);
+    switch (that.direction) {
+      case 0:
+        if () {
+          console.log(that.direction);
+          $("#ball").css({left: x+=1});
+          $("#ball").css({top: y-=1});
+        } else {
+          that.direction = 1;
+        }
+        break;
+        }
+
+});*/
+
+
+
+
 
 Ball.prototype.pointScored = function(){
 };
--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,20 +1,39 @@
 var board = new Board();
-
+var ball = new Ball();
 
 $('#start').on('click', function(){
   board.start();
   activatePaddle2();
-  var game = setInterval(updateState, intervalTime);
+  console.log("Hola");
+  //setInterval(function(){
+  ball.move();
+  //});
+  //setInterval(ball.move(), 100); // Al llamar a variable pierde la funcionalidad
+  //ball.move();
+  //var game = setInterval(updateState, intervalTime);
   renderGame();
 });
 
 function updateState(){
 }
 
-$(document).on('keydown', function(e){
-});
-
 function activatePaddle2() {
+  $(document).on('keydown', function(e){
+    var z = $("#paddle2").position().top;
+    console.log($("#paddle2").position());
+
+    if(event.keyCode === 38){
+      if(z <= 0){
+        return;
+      }
+      $("#paddle2").css({top: z-=13});
+    }else if(event.keyCode === 40){
+      if(z >= 340){
+        return;
+      }
+      $("#paddle2").css({top: z+=13});
+    }
+  });
 }
 
 function renderGame(){
