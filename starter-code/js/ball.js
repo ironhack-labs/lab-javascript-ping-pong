8413f8b0b24f63747332083f0d4efe6c154336ad
ferrodri <f.rodriguez.hervias@gmail.com>
2017-06-26 10:58:51 +0200

    entrega ferrodri ping pong

--- starter-code/js/ball.js
+++ starter-code/js/ball.js
@@ -1,18 +1,94 @@
-function Ball(x,y, paddle1, paddle2) {
+function Ball(x, y, paddleComputer, paddleUser) {
+  this.dir = 1;
+  this.x = 0;
+  this.y = 0;
 }
+Ball.prototype.randomDirection = function() {};
 
-Ball.prototype.randomDirection = function() {
+Ball.prototype.move = function() {
+  this.x = $("#ball").position().top;
+  this.y = $("#ball").position().left;
+  var that = this;
+  var intervalId = setInterval(function() {
+    var userX = $("#paddle-user").position().top + 360;
+    var userY = $("#paddle-user").position().left + 620;
+    switch (that.dir) {
+      case 0:
+        if (that.x > 0 && that.y < 1400) {
+          $("#ball").css({
+            top: that.x -= 10,
+            left: that.y += 10
+          });
+        }if((that.y == userY) && (that.x + 35 > userX) && (that.x < userX + 105)) {
+          that.dir = 1;
+        }if(that.y > userY + 35){
+          clearInterval(intervalId);
+          renderScore();
+        }
+        else if (that.x < 10) {
+          that.dir = 1;
+        } else if (that.y > 1390) {
+          that.dir = 3;
+        }
+        break;
+      case 1:
+        if (that.y < 1400 && that.x < 700) {
+          $("#ball").css({
+            top: that.x += 10,
+            left: that.y += 10
+          });
+        } if((that.y == userY) && (that.x + 35 > userX) && (that.x < userX + 105)) {
+          that.dir = 2;
+        }if(that.y > userY + 35){
+          clearInterval(intervalId);
+          renderScore();
+        }
+        else if (that.x > 690) {
+          that.dir = 0;
+        } else if (that.y > 1390) {
+          that.dir = 2;
+        }
+        break;
+      case 2:
+        if (that.x < 700 && that.y > 0) {
+          $("#ball").css({
+            top: that.x += 10,
+            left: that.y -= 10
+          });
+        } else if (that.x > 690) {
+          that.dir = 3;
+        } else if (that.y < 10) {
+          that.dir = 1;
+        }
+        break;
+      case 3:
+        if (that.x > 0 && that.y > 0) {
+          $("#ball").css({
+            top: that.x -= 10,
+            left: that.y -= 10
+          });
+        } else if (that.x < 10) {
+          that.dir = 2;
+        } else if (that.y < 10) {
+          that.dir = 0;
+        }
+        break;
+    }
+  }, 30);
 };
 
-Ball.prototype.move = function(){
-};
 
-Ball.prototype.pointScored = function(){
-};
+
+
+Ball.prototype.pointScored = function() {};
 
 // returns winner paddle or false
-Ball.prototype.winner = function(){
-};
+Ball.prototype.winner = function() {};
 
-Ball.prototype.restart = function(){
+Ball.prototype.restart = function() {
+  $("#ball").css({
+    top: this.x = 0,
+    left: this.y = 720
+  });
+  this.dir = 1;
 };
--- starter-code/js/paddle.js
+++ starter-code/js/paddle.js
@@ -1,7 +1,21 @@
-function Paddle(x,y) {
+function PaddleUser(x, y) {
+  this.x = 0;
 }
-Paddle.prototype.restart = function(){
+PaddleUser.prototype.restart = function() {
+  this.x = 0;
 };
 
-Paddle.prototype.hitBall = function(ball_y){
+PaddleUser.prototype.userMovement = function() {
+  $(document).on('keydown', function() {
+    this.x = $("#paddle-user").position().top;
+    if (event.keyCode == 40 && this.x < 270) {
+      $("#paddle-user").css({
+        top: this.x += 20 //down//
+      });
+    } else if (event.keyCode == 38 && this.x > -350) {
+      $("#paddle-user").css({
+        top: this.x -= 20 //up//
+      });
+    }
+  });
 };
