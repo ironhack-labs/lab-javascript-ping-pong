07baed0a0ce6250b0f21d532ac509e534992939f
Ander Galdeano <andergaldeano@gmail.com>
2017-08-24 21:57:05 +0200

    primer intento

--- starter-code/js/ball.js
+++ starter-code/js/ball.js
@@ -1,18 +1,116 @@
 function Ball(x,y, paddle1, paddle2) {
+  this.minTop = 10;
+  var alturaTablero = parseInt($("#board").css('height'));
+  var alturaPelota = parseInt($("#ball").css('height'));
+  this.maximillo = alturaTablero - alturaPelota ;
+
+
+  this.minLefth = 10;
+  var anchoTablero = parseInt($("#board").css('width'));
+  var anchoPelota = parseInt($("#ball").css('width'));
+  this.maximiliano = anchoTablero - anchoPelota ;
 }
 
-Ball.prototype.randomDirection = function() {
+// Ball.prototype.randomDirection = function() {
+// };
+//
+// Ball.prototype.move = function(){
+// };
+//
+// Ball.prototype.pointScored = function(){
+// };
+//
+// // returns winner paddle or false
+// Ball.prototype.winner = function(){
+// };
+//
+// Ball.prototype.restart = function(){
+// };
+
+
+Ball.prototype.arribaDerecha = function(){
+  var b = parseInt($("#ball").css('top'));
+  var x = this.minTop;
+
+
+  var b2 = parseInt($("#ball").css('left'));
+  // var x2 = this.minLefth;
+  // var maximLefth = this.maximilino;
+  var chocara = b>x;
+
+if(!chocara){
+  console.log("arriba derecha");
+  b -=1;
+  $("#ball").css('top', b +'px');
+  b2 +=1;
+  $("#ball").css('left', b2 +'px');
+  Ball.prototype.arribaDerecha();}
+  else{
+    Ball.prototype.abajoDerecha();}
 };
 
-Ball.prototype.move = function(){
+
+
+Ball.prototype.arribaIzquierda = function(){
+  var b = parseInt($("#ball").css('top'));
+  var x = this.minTop;
+
+
+  var b2 = parseInt($("#ball").css('left'));
+  // var x2 = this.minLefth;
+  // var maximLefth = this.maximilino;
+  var chocara = b<x;
+
+  if(!chocara){
+  console.log("arriba derecha");
+  b -=8;
+  $("#ball").css('top', b +'px');
+  b2 -=8;
+  $("#ball").css('left', b2 +'px');
+  Ball.prototype.arribaIzquierda();}
+  else{
+    Ball.prototype.abajoIzquierda();}
 };
 
-Ball.prototype.pointScored = function(){
+Ball.prototype.abajoDerecha = function(){
+  var b = parseInt($("#ball").css('top'));
+  var maximTop = this.maximillo;
+
+  var b2 = parseInt($("#ball").css('left'));
+  // var x2 = this.minLefth;
+  // var maximLefth = this.maximilino;
+  var chocara = b<maximTop;
+
+  if (!chocara){
+    console.log("arriba derecha");
+    b +=8;
+    $("#ball").css('top', b +'px');
+    b2 -=8;
+    $("#ball").css('left', b2 +'px');
+    Ball.prototype.abajoDerecha();}
+
+  else{
+    Ball.prototype.arribaDerecha();
+    }
 };
 
-// returns winner paddle or false
-Ball.prototype.winner = function(){
-};
+Ball.prototype.abajoIzquierda = function(){
+  var b = parseInt($("#ball").css('top'));
+  var maximTop = this.maximillo;
 
-Ball.prototype.restart = function(){
+  var b2 = parseInt($("#ball").css('left'));
+  // var x2 = this.minLefth;
+  // var maximLefth = this.maximilino;
+  var chocara = b<maximTop;
+
+  if (!chocara){
+    console.log("arriba derecha");
+    b -=8;
+    $("#ball").css('top', b +'px');
+    b2 +=8;
+    $("#ball").css('left', b2 +'px');
+    Ball.prototype.abajoIzquierda();}
+  else{
+      Ball.prototype.arribaIzquierda  ();
+    }
 };
--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,30 +1,100 @@
-var board = new Board();
+
+$( document ).ready(function() {
+  var keys = {};
+  var raqueta = new Paddle();
+
+   setInterval(checkControls, 30);
+
+    $(document).keydown(function(e){
+     keys[e.keyCode] = true;
+      }).keyup(function(e){
+      delete keys[e.keyCode];
+    });
+
+    function checkControls(){
+      console.log("entro checkcontrol");
+      if (keys[38])
+        raqueta.moveUp();
+      else if (keys[40])
+        raqueta.moveDown();
+    }
+
+    raqueta.moveDown();
+    raqueta.moveUp();
+
+// pelota
+//
+//
+    var pelota = new Ball();
+
+pelota.arribaIzquierda();
 
 
-$('#start').on('click', function(){
-  board.start();
-  activatePaddle2();
-  var game = setInterval(updateState, intervalTime);
-  renderGame();
 });
 
-function updateState(){
-}
 
-$(document).on('keydown', function(e){
-});
 
-function activatePaddle2() {
-}
 
-function renderGame(){
-}
 
-function renderScore(){
-}
 
-function renderBall(){
-}
 
-function renderPaddle(){
-}
+
+
+
+
+
+
+
+
+
+// var board = new Board();
+//
+
+// $( document ).ready(function() {
+//   var keys = {};
+//    setInterval(checkControls, 30);
+//
+//     $(document).keydown(function(e){
+//      keys[e.keyCode] = true;
+//       }).keyup(function(e){
+//       delete keys[e.keyCode];
+//         });
+//   Paddle.checkControls();
+//   Paddle.moveDown ();
+//   Paddle.moveUp ();
+//
+//
+// });
+
+
+
+
+
+
+// $('#start').on('click', function(){
+//   board.start();
+//   activatePaddle2();
+//   var game = setInterval(updateState, intervalTime);
+//   renderGame();
+// });
+//
+// function updateState(){
+// }
+//
+// $(document).on('keydown', function(e){
+// });
+//
+// function activatePaddle2() {
+// }
+//
+// function renderGame(){
+// }
+//
+// function renderScore(){
+// }
+//
+// function renderBall(){
+// }
+//
+// function renderPaddle(){
+// }
