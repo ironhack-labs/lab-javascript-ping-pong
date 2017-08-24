9a2e14c4f64d084be50dc9264bcc9a17c86a7984
Jimena Martin <jimena.mg@hotmail.com>
2017-08-24 21:03:25 +0200

    It moves

--- starter-code/js/ball.js
+++ starter-code/js/ball.js
@@ -1,10 +1,62 @@
 function Ball(x,y, paddle1, paddle2) {
+  this.control = 0;
+  this.positionX = x;
+  this.positionY = y;
+  this.againstPaddle1 = paddle1;
+  this.againstPaddle2 = paddle2;
+  this.direction = this.randomDirection();
 }
 
 Ball.prototype.randomDirection = function() {
+ var directions = ["leftup", "rightup", "rightdown", "leftdown"];
+ var picked =  Math.floor(Math.random()*4);
+ return directions[picked];
+
 };
 
 Ball.prototype.move = function(){
+
+var howUp = parseInt($(myBall).css("top"));
+var howLeft = parseInt($(myBall).css("left"));
+  if(howUp === 55){
+    this.control = 1;
+  }
+  if(howLeft === 1100){
+    this.control =  2;
+  }
+  if(howUp === 805){
+    this.control = 3;
+  }
+  if(howLeft === 50){
+    this.control = 0;
+  }
+
+  switch (this.control) {
+    case 0:
+      howUp -= 10;
+      howLeft +=10;
+      break;
+      case 1:
+      howUp +=10;
+      console.log(howLeft);
+      howLeft += 10;
+      break;
+      case 2:
+      howUp +=10;
+      console.log(howUp);
+      howLeft -=10;
+      break;
+      case 3:
+      howUp -=10;
+      howLeft-=10;
+      break;
+    default:
+
+  }
+$(myBall).css("top", howUp);
+$(myBall).css("left", howLeft);
+
+
 };
 
 Ball.prototype.pointScored = function(){
--- starter-code/js/paddle.js
+++ starter-code/js/paddle.js
@@ -1,7 +1,34 @@
 function Paddle(x,y) {
+  this.positionX = x;
+  this.positionY = y;
+
 }
 Paddle.prototype.restart = function(){
 };
 
 Paddle.prototype.hitBall = function(ball_y){
 };
+
+
+Paddle.prototype.renderPaddle = function(){
+
+};
+
+Paddle.prototype.moveUp = function(whichPaddle){
+  var howUp = parseInt($(whichPaddle).css("top"));
+  if(howUp > 10){
+  howUp -=10;
+  $(whichPaddle).css("top", howUp + "px");
+}
+
+};
+
+Paddle.prototype.moveDown = function(whichPaddle){
+  var howDown = parseInt($(whichPaddle).css("top"));
+  if(howDown < 560)
+  howDown +=10;
+  $(whichPaddle).css("top", howDown + "px");
+};
+
+function activatePaddle2() {
+}
