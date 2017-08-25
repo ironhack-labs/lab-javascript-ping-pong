14844ccb4120a60f5115a5017f032cb63c412416
trigoporres <santiagotrigoporres@gmail.com>
2017-08-25 11:26:55 +0200

    funciona las palas

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,30 +1,66 @@
-var board = new Board();
+//declaramos constructor
+var paddle = new Paddle();
+var keys = {};
 
 
-$('#start').on('click', function(){
-  board.start();
-  activatePaddle2();
-  var game = setInterval(updateState, intervalTime);
-  renderGame();
+$(document).ready(function (){
+
+    //almacena codigo tecla pulsada
+    $(document).on('keydown', function(e){
+     keys[e.keyCode] = true;
+      }).keyup(function(e){
+      delete keys[e.keyCode];
+    });
+
+    //intervalo que permita el movimiento
+    setInterval(function (){
+      if (keys[81]) {
+        paddle.movUp();
+      }
+      else if(keys[65]) {
+        paddle.movDown();
+      }
+
+
+    },30);
+
+
+
+
 });
 
-function updateState(){
-}
 
-$(document).on('keydown', function(e){
-});
 
-function activatePaddle2() {
-}
 
-function renderGame(){
-}
 
-function renderScore(){
-}
 
-function renderBall(){
-}
+// var board = new Board();
+//
+// $('#start').on('click', function(){
+//   board.start();
+//   activatePaddle2();
+//   var game = setInterval(updateState, intervalTime);
+//   renderGame();
+// });
+//
+// function updateState(){
+// }
+//
+// $(document).on('keydown', function(e){
+//   Paddle.movDown();
+// });
 
-function renderPaddle(){
-}
+// function activatePaddle2() {
+// }
+//
+// function renderGame(){
+// }
+//
+// function renderScore(){
+// }
+//
+// function renderBall(){
+// }
+//
+// function renderPaddle(){
+// }
